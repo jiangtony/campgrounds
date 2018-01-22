a07e3f3e2cb416c597ece2956db635be062433cd
jiangtony <tony.jiang34@gmail.com>
2018-01-21 21:23:58 -0500

    Added ability to write comments
    Added the routes to write comments and some styling for the info pages

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
 var express = require("express"),
 		app = express(),
 		bodyParser = require("body-parser"),
-		mongoose = require("mongoose");
+		mongoose = require("mongoose"),
+		Campground = require("./models/campground"),
+		Comment = require("./models/comment"),
+		seedDB = require("./seeds");
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + "/public"));
 mongoose.connect("mongodb://localhost/campgrounds");
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String,
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-
-// Campground.create(
-// {
-// 	name: "Salmon Creek",
-// 	image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
-// 	description: "Lots of salmon",
-// })
-
-// var campgrounds = [
-// 		{name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-// 		{name: "River Valley", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-// 		{name: "Granite Peak", image: "https://farm2.staticflickr.com/1086/882244782_d067df2717.jpg"},
-// 		{name: "Forest Hill", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"},
-// 		{name: "Goat's Rest", image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg"}
-// ];
+seedDB();
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+// INDEX route
 app.get("/campgrounds", function(req, res) {
 	Campground.find({}, function(err, allCampgrounds) {
 		if(err) {
-			console.log(error);
+			console.log(err);
 		} else {
-			res.render("index", {campgrounds: allCampgrounds});
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});
 		}
 	});
 });
 
+// CREATE route
 app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
@@ -55,27 +37,66 @@ app.post("/campgrounds", function(req, res) {
 	
 	Campground.create(newCampground, function(err, newlyCreated) {
 		if(err) {
-			console.log(error);
+			console.log(err);
 		} else {
 			res.redirect("/campgrounds");
 		}
 	});
 });
 
+// NEW route
 app.get("/campgrounds/new", function(req, res) {
-	res.render("new");
+	res.render("campgrounds/new");
 });
 
+// SHOW route
 app.get("/campgrounds/:id", function(req, res) {
 	// find the campground with id in database and render information
-	Campground.findByID(req.params.id, function(err, foundCampground) {
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
 		if(err) {
-			console.log(error);
+			console.log(err);
 		} else {
-			res.render("show", {campgrounds: foundCampground});
+			res.render("campgrounds/show", {campground: foundCampground});
 		}
 	});
 });
+
+///////////////////////
+// COMMENTS routes
+///////////////////////
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: foundCampground});
+		}
+	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+					res.redirect("/campgrounds");
+				} else {
+					foundCampground.comments.push(comment._id);
+					foundCampground.save();
+					res.redirect("/campgrounds/" + foundCampground._id);
+				}
+			});
+		}
+	});
+});
+///////////////////////////
+// End of comments routes
+///////////////////////////
+
 
 app.listen(3000, function() {
 	console.log("working");
